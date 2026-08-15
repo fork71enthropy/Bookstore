f449c5e1e88fa3405991e5817e1598dfc3ba2fd6
fork71enthropy <julientelook@gmail.com>
2026-08-16 01:39:17 +0200

    docker-compose.yml et docker-compose-prod.yml updated with security variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,8 +23,7 @@ env.read_env()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
-
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="django-insecure-build-only-placeholder-do-not-use-in-prod")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
